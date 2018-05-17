877ab5ff9faa628cef70e6585605516ceab7ca58
Patrick Coffey <patrickcoffey48@gmail.com>
2018-05-17 16:02:31 -0500

    docs(readme): lint-stage add readme

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   '**/*.{js,jsx,json,css}': ['prettier --write', 'git add'],
-  '*.emdaer.md': ['emdaer --yes', 'git add']
+  '*.emdaer.md': ['emdaer --yes', 'git add README.md']
 };
