38efe7f0ddb55a6d94c6e15d1f0c52cc720b2735
Patrick Coffey <patrickcoffey48@gmail.com>
2018-06-07 12:00:25 -0500

    test(experiences): test experiencesCreate/Edit reducers

--- src/reducers/experiences.test.js
+++ src/reducers/experiences.test.js
@@ -4,7 +4,11 @@
  */
 
 import reducer from './experiences';
-import { EXPERIENCES_FETCH_FOR_USER } from '../constants';
+import {
+  EXPERIENCES_FETCH_FOR_USER,
+  EXPERIENCES_CREATE,
+  EXPERIENCES_EDIT
+} from '../constants';
 
 describe('reducers->experiences', () => {
   it('Should return the initial state by default', () => {
@@ -28,8 +32,7 @@ describe('reducers->experiences', () => {
       reducer(undefined, {
         type: `${EXPERIENCES_FETCH_FOR_USER}_FAIL`,
         loading: false,
-        error,
-        payload: {}
+        payload: { error }
       })
     ).toMatchSnapshot();
   });
@@ -46,6 +49,101 @@ describe('reducers->experiences', () => {
             title: 'example experience'
           }
         ]
+      })
+    ).toMatchSnapshot();
+  });
+
+  it(`Should handle ${EXPERIENCES_CREATE}_LOADING`, () => {
+    expect(
+      reducer(undefined, {
+        type: `${EXPERIENCES_CREATE}_LOADING`,
+        loading: true,
+        error: null,
+        payload: {}
+      })
+    ).toMatchSnapshot();
+  });
+
+  it(`Should handle ${EXPERIENCES_CREATE}_FAIL`, () => {
+    const error = 'Error: failed to fetch experiences.';
+    expect(
+      reducer(undefined, {
+        type: `${EXPERIENCES_CREATE}_FAIL`,
+        loading: false,
+        payload: {
+          error
+        }
+      })
+    ).toMatchSnapshot();
+  });
+
+  it(`Should handle ${EXPERIENCES_CREATE}_SUCCESS`, () => {
+    expect(
+      reducer(undefined, {
+        type: `${EXPERIENCES_CREATE}_SUCCESS`,
+        loading: false,
+        error: null,
+        payload: [
+          {
+            id: 1,
+            title: 'example experience'
+          }
+        ]
+      })
+    ).toMatchSnapshot();
+  });
+
+  it(`Should handle ${EXPERIENCES_EDIT}_LOADING`, () => {
+    expect(
+      reducer(undefined, {
+        type: `${EXPERIENCES_EDIT}_LOADING`,
+        loading: true,
+        error: null,
+        payload: {}
+      })
+    ).toMatchSnapshot();
+  });
+
+  it(`Should handle ${EXPERIENCES_EDIT}_FAIL`, () => {
+    const error = 'Error: failed to fetch experiences.';
+    expect(
+      reducer(undefined, {
+        type: `${EXPERIENCES_EDIT}_FAIL`,
+        loading: false,
+        payload: { error }
+      })
+    ).toMatchSnapshot();
+  });
+
+  it(`Should handle ${EXPERIENCES_EDIT}_SUCCESS`, () => {
+    // Pass in mock state, this will assert that this reducer is injecting
+    // the updated experience at the right index.
+    const state = {
+      items: [
+        {
+          id: 0
+        },
+        {
+          id: 3
+        },
+        {
+          id: 1
+        },
+        {
+          id: 2
+        }
+      ]
+    };
+
+    expect(
+      reducer(state, {
+        type: `${EXPERIENCES_EDIT}_SUCCESS`,
+        loading: false,
+        error: null,
+        payload: {
+          id: 1,
+          title: 'example experience'
+        }
       })
     ).toMatchSnapshot();
   });
