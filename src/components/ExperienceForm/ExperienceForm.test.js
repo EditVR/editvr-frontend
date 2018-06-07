a4fe2e239aa1b5ff398afe1e6df67f1ac9ea0822
Patrick Coffey <patrickcoffey48@gmail.com>
2018-06-07 13:35:09 -0500

    test(experience): experience form edit mode test

--- src/components/ExperienceForm/ExperienceForm.test.js
+++ src/components/ExperienceForm/ExperienceForm.test.js
@@ -4,13 +4,15 @@
  */
 
 import React from 'react';
+import { Provider } from 'react-redux';
 import { MemoryRouter as Router } from 'react-router-dom';
+import { mount, shallow } from 'enzyme';
 import configureStore from 'redux-mock-store';
 import renderer from 'react-test-renderer';
 import ExperienceForm from './ExperienceForm.container';
 
 describe('<ExperienceForm />', () => {
-  it('Matches its snapshot', () => {
+  it('Can be a Create form for Experiences', () => {
     const store = configureStore()({
       user: {
         error: null,
@@ -30,5 +32,67 @@ describe('<ExperienceForm />', () => {
         )
         .toJSON()
     ).toMatchSnapshot();
+  });
+
+  it('Can be an Edit form for Experiences', () => {
+    const store = configureStore()({
+      user: {
+        error: null,
+        authentication: {
+          accessToken: 'test',
+          csrfToken: 'test'
+        }
+      },
+      experiences: {
+        items: [
+          {
+            id: '10',
+            title: 'test',
+            body: {
+              value: 'test'
+            },
+            field_experience_path: 'test'
+          }
+        ]
+      }
+    });
+
+    expect(
+      renderer
+        .create(
+          <Router>
+            <ExperienceForm store={store} experienceSlug="test" />
+          </Router>
+        )
+        .toJSON()
+    ).toMatchSnapshot();
+  });
+
+  it('Executes its submit handler as expected.', () => {
+    const submitFn = jest.fn();
+    const store = configureStore()({
+      user: {
+        error: null,
+        authentication: {
+          accessToken: 'test',
+          csrfToken: 'test'
+        }
+      }
+    });
+
+    const wrapper = mount(
+      shallow(
+        shallow(
+          <Provider store={store}>
+            <Router>
+              <ExperienceForm submitHandler={submitFn} store={store} />
+            </Router>
+          </Provider>
+        ).get(0)
+      ).get(0)
+    );
+
+    wrapper.find('form').simulate('submit');
+    expect(submitFn).toHaveBeenCalledTimes(1);
   });
 });
