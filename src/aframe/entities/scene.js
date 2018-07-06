b781f8cf5d7cf584bce2eeb8b26f92870b916098
Patrick Coffey <patrickcoffey48@gmail.com>
2018-07-06 10:56:38 -0500

    feat(dialog): spawn dialog components

--- src/aframe/entities/scene.js
+++ src/aframe/entities/scene.js
@@ -8,15 +8,18 @@
 import React from 'react';
 import aframe from 'aframe/src';
 
+require('@editvr/aframe-dialog-popup-component');
 require('aframe-look-at-component');
 require('../components/spawnSky');
 require('../components/spawnLinks');
+require('../components/spawnDialogs');
 require('../components/navLink');
 
 export default () => (
   <a-scene
     embedded
     spawn-links=""
+    spawn-dialogs=""
     inspector="url: https://aframe.io/releases/0.4.0/aframe-inspector.min.js"
   >
     <a-camera
