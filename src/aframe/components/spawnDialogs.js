4499e63d17a662d4bc2cf3c53c1d7ed8b2c34f13
Patrick Coffey <patrickcoffey48@gmail.com>
2018-07-19 10:22:59 -0500

    fix(dialogs): add body/title font/color, and add dialog images

--- src/aframe/components/spawnDialogs.js
+++ src/aframe/components/spawnDialogs.js
@@ -65,14 +65,32 @@ const spawnDialogs = {
           const e = document.createElement('a-entity');
           e.setAttribute('id', component.id);
           e.setAttribute('look-at', '#camera');
-          e.setAttribute('is-editable', true);
           e.setAttribute('position', { x, y, z });
-          e.setAttribute('dialog-popup', {
+
+          const dialogPopup = {
             title,
+            titleColor: 'white',
+            titleFont: 'roboto',
             body,
+            bodyColor: 'white',
+            bodyFont: 'roboto',
+            dialogBoxColor: '#127218',
             openIconImage,
             closeIconImage
-          });
+          };
+
+          if (component.field_image) {
+            const {
+              field_image: {
+                url: path,
+                links: { self }
+              }
+            } = component;
+            const url = new URL(self);
+            dialogPopup.image = `${url.origin}${path}`;
+          }
+
+          e.setAttribute('dialog-popup', dialogPopup);
           this.el.appendChild(e);
         });
     }
