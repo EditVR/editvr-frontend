4386dd3d4b022533b7a8b96767e1f514d6ece32b
Patrick Coffey <patrickcoffey48@gmail.com>
2018-09-28 17:17:23 -0500

    fix(config): remove loading of local config that may not exist

--- src/config/index.js
+++ src/config/index.js
@@ -3,24 +3,13 @@
  * Exports default config, overridden by local config.
  */
 
-let defaultConfig = require('./development.json');
+let config = require('./development.json');
 
 const { NODE_ENV } = process.env;
 
 // If this is a production environment, override with production settings.
 if (NODE_ENV === 'production') {
-  defaultConfig = require('./production.json'); // eslint-disable-line global-require
+  config = require('./production.json'); // eslint-disable-line global-require
 }
 
-// If local config exists, allow overrides.
-let localConfig;
-if (NODE_ENV === 'development') {
-  try {
-    // eslint-disable-next-line
-    localConfig = require('./local.json');
-  } catch (e) {
-    localConfig = {};
-  }
-}
-
-module.exports = { ...defaultConfig, ...localConfig };
+module.exports = config;
